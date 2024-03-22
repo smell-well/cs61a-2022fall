ba6a3faf236a6cfc7fb20d01e7c5c547f9e3a9a0
YMK <mengke.yang@Outlook.com>
2024-03-22 23:09:30 +0800

    init repo, complete lab00 and hw01

---
+++ Lab/lab00/lab00.py
@@ -0,0 +1,11 @@
+def twenty_twenty_two():
+    """Come up with the most creative expression that evaluates to 2022,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_two()
+    2022
+    """
+    x = 1000
+    x *= 2
+    x += 22
+    return x
